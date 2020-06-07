248b385fcac0977ca452c302bbe3c2d87e6aeb00
Jeff Maughan <geophray@gmail.com>
2020-06-06 19:01:51 -0700

    Add form to react for searching tasty api

--- client/App.jsx
+++ client/App.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      q: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleQuery = this.handleQuery.bind(this);
   }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleQuery(e) {
+    e.preventDefault();
+    console.log(this.state.q);
+  }
+
 
   render() {
     return (
-      <h1>My app is rendering!</h1>
+      <form onSubmit={this.handleQuery}>
+        <label>
+          Find new recipes:
+          <input type="text" name="q" value={this.state.q} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   };
 };
