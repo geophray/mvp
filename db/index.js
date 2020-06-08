9cabba93fbfebdf0db804f198f0067dd01ed6785
Jeff Maughan <geophray@gmail.com>
2020-06-08 08:34:53 -0700

    Stub out recipe db methods

---
+++ db/index.js
@@ -0,0 +1,31 @@
+const pool = require('./config.js');
+
+module.exports = {
+  recipes: {
+    getAllSavedRecipes() {
+      // retrieve all my saved recipes
+    },
+
+    getRecipeById(id) {
+
+    },
+
+    addNewRecipe(recipe) {
+      // add a new recipe to the database
+    },
+
+    updateRecipe() {
+
+    },
+
+    deleteRecipe(id) {
+
+    },
+  },
+  users: {
+
+  },
+  reviews: {
+
+  },
+}
