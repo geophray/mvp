1dd4175a5e771c047f708e42cbb8a5f3bb4fe8fc
Jeff Maughan <geophray@gmail.com>
2020-06-08 08:50:50 -0700

    Add CRUD db methods for recipes

--- db/index.js
+++ db/index.js
@@ -2,24 +2,49 @@ const pool = require('./config.js');
 
 module.exports = {
   recipes: {
-    getAllSavedRecipes() {
+    getAllSavedRecipes(cb) {
       // retrieve all my saved recipes
+      const text = 'SELECT * FROM recipes';
+      pool.query(text, (err, res) => {
+        cb(err, res);
+      })
     },
 
-    getRecipeById(id) {
-
+    getRecipeById(id, cb) {
+      const text = 'SELECT * FROM recipes WHERE id = $1';
+      const values = [id];
+      pool.query(text, values, (err, res) => {
+        cb(err, res);
+      })
     },
 
-    addNewRecipe(recipe) {
+    addNewRecipe(recipe, cb) {
       // add a new recipe to the database
+      const { name, thumbnail_url, description, backstory, instructions, ingredients } = recipe;
+      const text = 'INSERT INTO recipes (name, thumbnail_url, description, backstory, instructions, ingredients) VALUES ($1, $2, $3, $4, $5, $6)';
+      const values = [name, thumbnail_url, description, backstory, instructions, ingredients];
+      pool.query(text, values, (err, res) => {
+        cb(err, res);
+      });
     },
 
-    updateRecipe() {
-
+    updateRecipe(id, updated, cb) {
+      // Update a recipe by id.
+      const { name, thumbnail_url, description, backstory, instructions, ingredients } = updated;
+      const text = 'UPDATE recipes SET name = $1, thumbnail_url = $2, description = $3, backstory = $4, instructions = $5, ingredients = $6)';
+      const values = [name, thumbnail_url, description, backstory, instructions, ingredients];
+      pool.query(text, values, (err, res) => {
+        cb(err, res);
+      })
     },
 
-    deleteRecipe(id) {
-
+    deleteRecipe(id, cb) {
+      // Deletes a recipe by id
+      const text = 'DELETE FROM recipes WHERE id = $1';
+      const values = [id];
+      pool.query(text, values, (err, res) => {
+        cb(err, res);
+      })
     },
   },
   users: {
