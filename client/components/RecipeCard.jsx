f8870930f4f77e3f781c4719003117c9feaff76f
Jeff Maughan <geophray@gmail.com>
2020-06-08 18:51:27 -0700

    Add view click handler to RecipeCard component

--- client/components/RecipeCard.jsx
+++ client/components/RecipeCard.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import styles from '../css/recipecard.css';
 
-const RecipeCard = ({ recipe }) => {
+const RecipeCard = ({ recipe, view }) => {
   return(
-    <div className="recipe-card">
+    <div className="recipe-card" onClick={() => view(recipe)}>
       <div className={styles['recipe-thumbnail']} style={{background: `url(${recipe.thumbnail_url})`}}></div>
       <div className={styles['recipe-card-text']}>
         <h3>{recipe.name}</h3>
