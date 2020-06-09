fbaf0a48f15d153af820a72672d02b60d5e5de42
Jeff Maughan <geophray@gmail.com>
2020-06-08 19:35:46 -0700

    Refine recipe styles

--- client/components/Recipe.jsx
+++ client/components/Recipe.jsx
@@ -16,30 +16,35 @@ const Recipe = ({ recipe, close }) => {
     return <IngredientsList key={component.position} component={component} />;
   });
   return (
-    <div className={styles['lightbox-bg']}>
-      <div className={styles['close-lightbox']} onClick={close} >&#10008;</div>
-      <div className={styles['lightbox-content-wrapper']}>
-        <div className={styles['recipe-photo']} style={{background: `url(${recipe.thumbnail_url})`}}>
+    <>
+      <div className={styles['lightbox-bg']}>
+        <div className={styles['lightbox-content-wrapper']}>
+
+          <span className={styles['close-lightbox']} onClick={close} >&#10008;</span>
+          <div className={styles['recipe-photo']} style={{background: `url(${recipe.thumbnail_url})`}}>
+          </div>
+          <div className={styles['recipe-content']}>
+            <h2>{recipe.name}</h2>
+            {recipe.backstory === '' ? null :
+              <>
+                <h3>About This Recipe</h3>
+                <p>{recipe.backstory}</p>
+              </>
+            }
+            {recipe.description === '' ? null :
+              <>
+                <h3>Description</h3>
+                <p>{recipe.description}</p>
+              </>
+            }
+            <h3>Ingredients</h3>
+            {ingredientsList}
+            <h3>Instructions</h3>
+            {instructionSteps}
+          </div>
         </div>
-        <h2>{recipe.name}</h2>
-        {recipe.backstory === '' ? null :
-          <>
-            <h3>About This Recipe</h3>
-            <p>{recipe.backstory}</p>
-          </>
-        }
-        {recipe.description === '' ? null :
-          <>
-            <h3>Description</h3>
-            <p>{recipe.description}</p>
-          </>
-        }
-        <h3>Ingredients</h3>
-        {ingredientsList}
-        <h3>Instructions</h3>
-        {instructionSteps}
       </div>
-    </div>
+    </>
   )
 };
 
